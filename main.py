f514effc30764f60349465f4e48e1e36b7280465
yosider <yosider.on.earth@gmail.com>
2023-09-13 12:45:38 +0900

    add first_page/last_page, formatting

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ from gyazo import Api
 from pdf2image import convert_from_path
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description="Convert PDF pages to images and upload them to Gyazo.")
+parser = argparse.ArgumentParser(
+    description="Convert PDF pages to images and upload them to Gyazo."
+)
 parser.add_argument("pdf", type=str, help="The name of the PDF file to convert")
+parser.add_argument("--dpi", type=int, default=300, help="The DPI of the output image")
+parser.add_argument(
+    "--first_page", type=int, default=1, help="The first page to convert"
+)
+parser.add_argument(
+    "--last_page", type=int, default=None, help="The last page to convert"
+)
 args = parser.parse_args()
 
 # load env: PDF_DIR, GYAZO_API_TOKEN
@@ -18,7 +27,12 @@ load_dotenv()
 client = Api(access_token=os.environ.get("GYAZO_API_TOKEN"))
 
 print("converting...")
-images = convert_from_path(os.environ.get("PDF_DIR") + args.pdf + ".pdf", dpi=300)
+images = convert_from_path(
+    os.environ.get("PDF_DIR") + args.pdf + ".pdf",
+    dpi=args.dpi,
+    first_page=args.first_page,
+    last_page=args.last_page,
+)
 
 urls = []
 for i, image in enumerate(tqdm(images, desc="uploading...")):
@@ -28,7 +42,11 @@ for i, image in enumerate(tqdm(images, desc="uploading...")):
         img_byte_data = img_byte_arr.getvalue()
 
         # upload to Gyazo
-        url = client.upload_image(img_byte_data).url.replace("i.gyazo.com", "gyazo.com").replace(".png", "")
+        url = (
+            client.upload_image(img_byte_data)
+            .url.replace("i.gyazo.com", "gyazo.com")
+            .replace(".png", "")
+        )
         urls.append(url)
 
 # copy to clipboard
